1d1fbd173ad3471e6f489e50d6a8e92f4cfe25bb
Czipó Bence <czipobence@gmail.com>
2017-10-25 16:11:00 +0200

    Unit appends verify (left and right)

--- FormalLanguages/Languages/languageWithFunction.scala
+++ FormalLanguages/Languages/languageWithFunction.scala
@@ -7,21 +7,26 @@ case class Lang[T](f: List[T] => Boolean) {
     def searchSplitIter(that: Lang[T], n: BigInt, l: List[T]): Boolean = {
       require(n <= l.size && n >= 0)
       if (this.contains(l.take(n)) && that.contains(l.drop(n))) true else
-      if (n == 0) false else
-      searchSplitIter(that,n-1,l)
+      if (n == l.size) false else
+      searchSplitIter(that,n+1,l)
+    } ensuring { res =>
+      !res ==> forall( (i: BigInt) => i <= n ==> !(this.contains(l.take(i)) && that.contains(l.drop(i))) )
     }
 
     def combine(that: Lang[T]): Lang[T] = {
-      Lang[T](l => searchSplitIter(that, l.size, l))
+      Lang[T](l => searchSplitIter(that, 0, l))
     }.ensuring {res =>
-      forall((t1: List[T]) => this.contains(t1) ==> forall((t2: List[T]) => that.contains(t2) ==> res.contains(t1 ++ t2)) )
+      forall((t1: List[T], t2: List[T]) =>
+        (this.contains(t1) && that.contains(t2)) ==> res.contains(t1 ++ t2)
+        )
     }
 
     // How could we define equality otherwise, there is no way that this would verify
-    // x => f(x) =? x => exists n. f(x.take(n)) && unit(x.drop(n))  
-    def == (that: Lang[T]): Lang[T] = {
+    // x => f(x) =? x => exists n. f(x.take(n)) && unit(x.drop(n))
+    def isEqual (that: Lang[T]): Boolean = {
       forall ( (x: List[T]) => this.contains(x) ==> that.contains(x) ) &&
       forall ( (x: List[T]) => that.contains(x) ==> this.contains(x) )
+      //maybe forall ( (x: List[T]) => this.contains(x) == that.contains(x) ) but thats nto easier for the solver
     }
 
   def contains(word: List[T]): Boolean = f(word)
@@ -33,19 +38,19 @@ object LangSuite {
   def nullLang[T](): Lang[T] = Lang[T](l => false)
 
   def rightUnitCombine[T](l1: Lang[T]): Boolean = {
-    l1.combine(unitLang[T]()) == l1
-  }
+    l1.combine(unitLang[T]()) isEqual l1
+  }.holds
 
   def leftUnitCombine[T](l1: Lang[T]): Boolean = {
-    unitLang[T]().combine(l1) == l1
-  }
+    unitLang[T]().combine(l1) isEqual l1
+  }.holds
 
 
   def rightNullCombine[T](l1: Lang[T]): Boolean = {
-    l1.combine(nullLang[T]()) == nullLang[T]()
-  }
+    l1.combine(nullLang[T]()) isEqual nullLang[T]()
+  }.holds
 
   def leftNullCombine[T](l1: Lang[T]): Boolean = {
-    nullLang[T]().combine(l1) == nullLang[T]()
-  }
+    nullLang[T]().combine(l1) isEqual nullLang[T]()
+  }.holds
 }
--- FormalLanguages/Languages/languageWithList.scala
+++ FormalLanguages/Languages/languageWithList.scala
@@ -4,57 +4,37 @@ import stainless.proof._
 import stainless.annotation._
 
 case class Lang[T](list: List[List[T]]) {
-
-  def appendToAll(suffix: List[T], l: List[List[T]]): List[List[T]] = {
-    l match {
-      case Nil() => Nil[List[T]]()
-      case Cons(x,xs) => (x ++ suffix) :: appendToAll(suffix, xs)
-    }
-  }.ensuring {res: List[List[T]] => res.size == l.size &&
-    !l.isEmpty ==> res.contains(l.head ++ suffix)/*res.forall(x => x.drop(x.size -suffix.size) == suffix )*/}
-
-    def appendToAllSplit(suffix: List[T], l: List[List[T]]): Boolean = {
+    /*def appendToAllSplit(suffix: List[T], l: List[List[T]]): Boolean = {
       l match {
         case Nil() => true
-        case x :: xs => appendToAll(suffix, l) == (x ++ suffix) :: appendToAll(suffix, xs)
+        case x :: xs => appendToAll(l, suffix) == (x ++ suffix) :: appendToAll(xs, suffix)
       }
     }.holds
 
   def appendToAllLemma(suffix: List[T], prefix: List[T], list: List[List[T]]): Boolean = {
     require(list.contains(prefix))
-    appendToAll(suffix, list).contains(prefix ++ suffix)
+    appendToAll(list, suffix).contains(prefix ++ suffix)
   }.holds
 
   def appendToAllLemma2(suffix: List[T], prefix: List[T], list: List[List[T]]): Boolean = {
     require(list.contains(prefix))
-    appendToAll(suffix, list).contains(prefix ++ suffix) because {
+    appendToAll(list, suffix).contains(prefix ++ suffix) because {
       list match {
-        case Nil() => check {appendToAll(suffix, list).contains(prefix ++ suffix)}
-        case Cons(prefix, _) => check {appendToAll(suffix, list).contains(prefix ++ suffix)}
+        case Nil() => check {appendToAll(list, suffix).contains(prefix ++ suffix)}
+        case Cons(prefix, _) => check {appendToAll(list, suffix).contains(prefix ++ suffix)}
         case Cons(x,xs) => {
-          appendToAll(suffix, Cons(x, xs)).contains(prefix ++ suffix)                                 ==| appendToAllSplit(suffix, list)  |
-          ((x ++ suffix) :: appendToAll(suffix, xs)).contains(prefix ++ suffix)                       ==| trivial                         |
-          ((x ++ suffix) == (prefix ++ suffix) || appendToAll(suffix, xs).contains(prefix ++ suffix)) ==| trivial                         |
-          appendToAll(suffix, xs).contains(prefix ++ suffix)                                          ==| trivial                         |
+          appendToAll(Cons(x, xs), suffix).contains(prefix ++ suffix)                                 ==| appendToAllSplit(suffix, list)  |
+          ((x ++ suffix) :: appendToAll(xs, suffix)).contains(prefix ++ suffix)                       ==| trivial                         |
+          ((x ++ suffix) == (prefix ++ suffix) || appendToAll(xs, suffix).contains(prefix ++ suffix)) ==| trivial                         |
+          appendToAll(xs, suffix).contains(prefix ++ suffix)                                          ==| trivial                         |
           appendToAllLemma2(suffix, prefix, xs)
         }
       }
     }.qed
-  }.holds
-
-  def createCombinedList(lst: List[List[T]]): List[List[T]] = {
-    lst match {
-      case Nil() => Nil[List[T]]()
-      case Cons(x,xs) => appendToAll(x, this.list) ++ createCombinedList(xs)
-    }
-  }.ensuring {res: List[List[T]] => res.size == this.list.size * lst.size}
-
-  /*def createCombinedListSizeLemma(lst: List[List[T]]):Boolean = {
-    createCombinedList(lst).size == this.list.size * lst.size
   }.holds*/
 
   def combine(that: Lang[T]): Lang[T] = {
-    Lang[T](createCombinedList(that.list))
+    Lang[T](Lang.combineLists(this.list, that.list))
   }.ensuring {res =>
     this.list.forall(l1 => that.list.forall(l2 => res.contains(l1 ++ l2)))
   }
@@ -78,61 +58,144 @@ case class Lang[T](list: List[List[T]]) {
     this.list.content == that.list.content
   }
 
-
-
   def contains(word: List[T]): Boolean = list.contains(word)
+}
 
-  def unitLang (): Lang[T] = Lang[T](List(Nil()))
+object Lang {
+  def appendToAll[T](l: List[List[T]], suffix: List[T]): List[List[T]] = {
+    l match {
+      case Nil() => Nil[List[T]]()
+      case Cons(x,xs) => (x ++ suffix) :: appendToAll(xs, suffix)
+    }
+  }.ensuring {res: List[List[T]] => res.size == l.size &&
+    !l.isEmpty ==> res.contains(l.head ++ suffix)}
 
-  def nullLang(): Lang[T] = Lang[T](Nil())
+  def prependToAll[T](prefix: List[T], l: List[List[T]]): List[List[T]] = reverseAll(appendToAll(reverseAll(l) , prefix.reverse))
 
-  def rightUnitCombine(l1: Lang[T]): Boolean = {
-    l1.combine(unitLang()) == l1
+  def reverseAll[T](l: List[List[T]]): List[List[T]] = l match {
+    case Nil() => Nil[List[T]]
+    case Cons(x,xs) => x.reverse :: reverseAll(xs)
+  }
+
+  def prependToAllMap[T](prefix: List[T], l: List[List[T]]): List[List[T]] = appendToAll(l.map(lst => lst.reverse) , prefix.reverse).map(lst => lst.reverse)
+
+  def combineLists[T](thisList: List[List[T]], thatList: List[List[T]]): List[List[T]] = {
+    thatList match {
+      case Nil() => Nil[List[T]]()
+      case Cons(x,xs) => appendToAll(thisList, x) ++ combineLists(thisList, xs)
+    }
+  }.ensuring {res: List[List[T]] => res.size == thisList.size * thatList.size}
+
+}
+
+import Lang._
+
+object LangSpecs {
+  def prependToAllLemmaMap[T](prefix: List[T], l: List[List[T]]): Boolean = {
+    prependToAllMap(prefix,l) == appendToAll(l.map(lst => lst.reverse) , prefix.reverse).map(lst => lst.reverse)
   }.holds
 
-  def leftUnitCombine(l1: Lang[T]): Boolean = {
-    unitLang().combine(l1) == l1 because {
-      l1 match {
-        case Lang(Nil()) => check {unitLang().combine(l1) == l1}
-        case Lang(x :: xs) => {
-          check {(unitLang().combine(x ::Lang[T](xs))) == (Lang[T](appendToAll(x, unitLang().list)) ++ (unitLang() combine Lang[T](xs)))} && combineDistributive(x, unitLang, Lang[T](xs)) &&
-          check {(Lang[T](appendToAll(x, unitLang().list)) ++ (unitLang() combine  Lang[T](xs))) == (Lang[T](List(x)) ++ (unitLang() combine  Lang[T](xs)))} &&
-          check {(Lang[T](List(x)) ++ (unitLang() combine  Lang[T](xs))) == (Lang[T](List(x)) ++  Lang[T](xs))} && leftUnitCombine(Lang[T](xs)) &&
-          check {(Lang[T](List(x)) ++  Lang[T](xs)) == (Lang[T](List(x) ++  xs))} &&
-          check {Lang[T](List(x) ++ xs) == Lang[T](x :: xs) }
-        }
-      }
+  def prependToAllLemma[T](prefix: List[T], l: List[List[T]]): Boolean = {
+    prependToAll(prefix,l) == reverseAll(appendToAll(reverseAll(l) , prefix.reverse))
+  }.holds
+
+
+  //the equals operator is symmectric, which means that if a == b than b==a
+  def equalsAssoc[T](l1: Lang[T], l2: Lang[T]): Boolean = {
+    (l1 == l2) == (l2 == l1)
+  }.holds
+
+  //It should verify by definition
+  // todo move lemmas to static object
+  def combinationLemma[T](l1: Lang[T], l2: Lang[T]): Boolean = {
+    (l1 combine l2) == Lang[T](combineLists(l1.list, l2.list))
+  }.holds
+
+  def reverseCombinationLemma[T](l1: Lang[T], l2: Lang[T]): Boolean = {
+    Lang[T](combineLists(l1.list, l2.list)) == (l1 combine l2) because {
+      combinationLemma(l1,l2) && equalsAssoc((l1 combine l2), Lang[T](combineLists(l1.list, l2.list)))
     }
   }.holds
 
-  def leftUnitCombineFormatted(l1: Lang[T]): Boolean = {
-    unitLang().combine(l1) == l1 because {
+  def unitLang[T](): Lang[T] = Lang[T](List(Nil()))
+
+  def nullLang[T](): Lang[T] = Lang[T](Nil())
+
+  def rightUnitCombine[T](l1: Lang[T]): Boolean = {
+
+    l1.combine(unitLang()) == l1 because {
       l1 match {
-        case Lang(Nil()) => check {unitLang().combine(l1) == l1}
+        case Lang(Nil()) => check {l1.combine(unitLang()) == l1}
         case Lang(x :: xs) => {
-          (unitLang().combine(x ::Lang[T](xs))) == (Lang[T](appendToAll(x, unitLang().list)) ++ (unitLang() combine Lang[T](xs)))                     ==| combineDistributive(x, unitLang, Lang[T](xs))  |
-          (Lang[T](appendToAll(x, unitLang().list)) ++ (unitLang() combine  Lang[T](xs))) == (Lang[T](List(x)) ++ (unitLang() combine  Lang[T](xs)))  ==| trivial                                        |
-          (Lang[T](List(x)) ++ (unitLang() combine  Lang[T](xs))) == (Lang[T](List(x)) ++  Lang[T](xs))                                               ==| leftUnitCombine(Lang[T](xs))                   |
-          (Lang[T](List(x)) ++  Lang[T](xs)) == (Lang[T](List(x) ++  xs))                                                                             ==| trivial                                        |
-          Lang[T](List(x) ++ xs) == Lang[T](x :: xs)
+          Lang[T](x :: xs).combine(unitLang())                                          ==| trivial                                             |
+          (x:: Lang[T](xs)).combine(unitLang())                                         ==| combineDistributiveLeft(x, Lang[T](xs), unitLang()) |
+          Lang[T](prependToAll(x, unitLang().list)) ++ (Lang[T](xs) combine unitLang()) ==| prependToUnitLang(x)                                |
+          Lang[T](List(x)) ++ (Lang[T](xs) combine unitLang())                          ==| rightUnitCombine(Lang[T](xs))                       |
+          Lang[T](List(x)) ++ Lang[T](xs)                                               ==| trivial                                             |
+          Lang[T](List(x) ++ xs)                                                        ==| trivial                                             |
+          Lang[T](x :: xs)
         }.qed
       }
     }
   }.holds
 
-  def rightNullCombine(l1: Lang[T]): Boolean = {
-    l1.combine(nullLang()) == nullLang()
+  def prependToUnitLang[T](prefix: List[T]): Boolean = {
+    prependToAll(prefix, unitLang().list) == List(prefix) because {
+      prependToAll(prefix, unitLang().list)                                                                 ==| trivial |
+      prependToAll(prefix, List[List[T]](Nil[T]()))                                                         ==| prependToAllLemma(prefix, List[List[T]](Nil[T]())) |
+      reverseAll(appendToAll(reverseAll(List[List[T]](Nil[T]())), prefix.reverse))                          ==| trivial |
+      reverseAll(appendToAll(List[List[T]](Nil[T]()),prefix.reverse))                                       ==| trivial |
+      reverseAll(List(prefix.reverse))                                                                      ==| trivial |
+      List(prefix.reverse.reverse)                                                                          ==| ListSpecs.reverseReverse(prefix) |
+      List(prefix)
+    }.qed
   }.holds
 
-  def leftNullCombine(l1: Lang[T]): Boolean = {
-    nullLang().combine(l1) == nullLang()
+  //reverseAll for SingleList, actually trivial
+  def reverseAllSingleItem[T](list: List[T]): Boolean = {
+    reverseAll(List(list)) == List(list.reverse)
   }.holds
 
-  def associativity(l1: Lang[T], l2: Lang[T], l3:Lang[T]): Boolean = {
+  def leftUnitCombine[T](l1: Lang[T]): Boolean = {
+    unitLang().combine(l1) == l1 because {
+      l1 match {
+        case Lang(Nil()) => check {unitLang().combine(l1) == l1}
+        case Lang(x :: xs) => {
+          (unitLang().combine(x ::Lang[T](xs)))                                           ==| combineDistributiveRight(unitLang(), x, Lang[T](xs))  |
+          (Lang[T](appendToAll(unitLang().list, x)) ++ (unitLang() combine  Lang[T](xs))) ==| trivial                                             |
+          (Lang[T](List(x)) ++ (unitLang() combine  Lang[T](xs)))                         ==| leftUnitCombine(Lang[T](xs))                        |
+          (Lang[T](List(x)) ++  Lang[T](xs))                                              ==| trivial                                             |
+          Lang[T](List(x) ++ xs)                                                          ==| trivial                                             |
+          Lang[T](x :: xs)
+        }.qed
+      }
+    }
+  }.holds
+
+  def rightNullCombine[T](l1: Lang[T]): Boolean = {
+    l1.combine(nullLang[T]()) == nullLang[T]()
+  }.holds
+
+  def leftNullCombine[T](l1: Lang[T]): Boolean = {
+    nullLang[T]().combine(l1) == nullLang[T]()
+  }.holds
+
+  def associativity[T](l1: Lang[T], l2: Lang[T], l3:Lang[T]): Boolean = {
     (l1 combine (l2 combine l3)) == ((l1 combine l2) combine l3)
   }.holds
 
-  def combineDistributive(w: List[T], l1:Lang[T], l2: Lang[T]): Boolean = {
-    (l1 combine (w :: l2)) == (Lang[T](appendToAll(w, l1.list)) ++ (l1 combine l2))
+  def combineDistributiveRight[T](l1: Lang[T], w: List[T], l2: Lang[T]): Boolean = {
+    ((l1 combine (w :: l2)) == (Lang[T](appendToAll(l1.list, w)) ++ (l1 combine l2))) because {
+      (l1 combine (w :: l2))                                                      ==| combinationLemma(l1, w::l2) |
+      Lang[T](combineLists(l1.list, (w :: l2).list))                              ==| trivial |
+      Lang[T](combineLists(l1.list, w :: l2.list))                                ==| trivial |
+      Lang[T](appendToAll(l1.list, w) ++ combineLists(l1.list, l2.list))          ==| trivial |
+      Lang[T](appendToAll(l1.list, w)) ++ Lang[T](combineLists(l1.list, l2.list)) ==| reverseCombinationLemma(l1, l2) |
+      Lang[T](appendToAll(l1.list, w)) ++ (l1 combine l2)
+    }.qed
+  }.holds
+
+  def combineDistributiveLeft[T](w: List[T], l1: Lang[T], l2: Lang[T]): Boolean = {
+    ( (w ::  l1) combine l2) == Lang[T](prependToAll(w, l2.list)) ++ (l1 combine l2)
   }.holds
 }
