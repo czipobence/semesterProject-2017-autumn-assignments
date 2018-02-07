24698ece484b045b4a1e73e07be303c012417f76
Czipó Bence <czipobence@gmail.com>
2018-02-07 17:52:31 +0100

    playing around wtih high order theorems

--- HighOrderTheorems/HighOrder.scala
+++ HighOrderTheorems/HighOrder.scala
@@ -3,35 +3,55 @@ import stainless.collection._
 import stainless.proof._
 import stainless.annotation._
 
+/**
+ * This code snippet is to test if Stainless can verify theorems about functions
+ * I called them High Order Theorems after high order function (map, foldr,...)
+ */
+
 object HighOrder {
+
+  // If the order of two operations are interchangeable in case of two
+  // paramaters, so does it in case of three.
   def commutativityTheorem[T](f: T => T, g: (T,T) => T): Boolean = {
     require(forall((a: T, b: T) => f(g(a,b)) == g(f(a), f(b))  )  )
     forall((a: T, b: T, c: T) => f(g(g(a,b),c)) == g(g(f(a), f(b)), f(c))  )
   }.holds
+}
 
-  def identity(a: List[BigInt]): List[BigInt] = {
-    a match {
-      case Nil() => Nil()
-      case x :: xs => (x + 1) :: identity(xs)
+// A structure with two operations
+object SampleStructure {
+    // The one argument operation. First I experimented with identity, but
+    // then Stainless could verify it on its own so I tweaked it a bit, however,
+    // the name remains
+    def identity(a: List[BigInt]): List[BigInt] = {
+      a match {
+        case Nil() => Nil()
+        case x :: xs => (x + 1) :: identity(xs)
+      }
     }
-  }
 
-  def concat[T](a: List[T], b: List[T]) = {
-    a ++ b
-  }
+    // Two argument operation (g)
+    def concat[T](a: List[T], b: List[T]) = {
+      a ++ b
+    }
 
-  def concatIdentityDistributive(a: List[BigInt], b: List[BigInt]): Boolean = {
-    identity(concat(a,b)) == concat(identity(a), identity(b))
-  }.holds
+    // Theorem about the interchangeabilty of the order of operations
+    @induct
+    def concatIdentityDistributive(a: List[BigInt], b: List[BigInt]): Boolean = {
+      (identity(concat(a,b)) == concat(identity(a), identity(b)))
+    }.holds
 
+}
 
-  def concatIdentityDistributiveForall(): Boolean = {
-    forall((a: List[BigInt], b: List[BigInt]) => identity(concat(a,b)) == concat(identity(a), identity(b)) because {concatIdentityDistributive(a,b)})
-  }.holds
+import HighOrder._
+import SampleStructure._
 
+// Example how this could be used to prove a real lemma
+object Demo {
   def useHighOrderTheorem(a: List[BigInt], b: List[BigInt], c: List[BigInt]): Boolean = {
+    check {forall((a: List[BigInt], b: List[BigInt]) => identity(concat(a,b)) == concat(identity(a), identity(b)) because {concatIdentityDistributive(a,b)})}
     identity(concat(concat(a,b),c)) == concat(concat(identity(a), identity(b)), identity (c)) because {
-      commutativityTheorem[List[BigInt]](identity, concat) because {concatIdentityDistributiveForall()}
+      commutativityTheorem[List[BigInt]](identity, concat)
     }
   }.holds
 }
